7ddb53e40d8d01bfd0bc0171f1e7e59eb314b80c
shengqiangdd <71962734+shengqiangdd@users.noreply.github.com>
2026-06-03 16:57:55 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -132,8 +132,8 @@ git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall2 packag
 
 # Themes
 #git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
-#git clone --depth=1 -b main https://github.com/kiddin9/op-packages package/luci-theme-argon
-#git clone --depth=1 -b main https://github.com/kiddin9/op-packages package/luci-app-argon-config
+#git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+#git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 #git_sparse_clone main https://github.com/kiddin9/op-packages luci-theme-argon
 #git_sparse_clone main https://github.com/kiddin9/op-packages luci-app-argon-config
 #git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
